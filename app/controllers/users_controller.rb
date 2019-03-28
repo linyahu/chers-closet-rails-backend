d066e59c4b2c1809a532a5e4e8af265626f278d9
github username <github email address>
2019-03-28 15:24:52 -0400

    fake auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :username, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :items, :outfits, :outfit_items
+  resources :items, :outfits, :outfit_items, :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :items
